4462e8d6028a900999ab8e66195457d0ea763834
Lucas-ros4 <lucasrosallrs@gmail.com>
2026-02-15 07:01:11 -0300

    Primeiro commit - programa conta com apeans um cliente, futuramente pretendo integrar 2 clientes e uma interface grafica

---
+++ ChatCliente/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        TcpClient cliente = new TcpClient("127.0.0.1", 5000);
+
+        Console.WriteLine("Conectado ao servidor!");
+
+        NetworkStream stream = cliente.GetStream();
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream);
+        writer.AutoFlush = true;
+
+        new Thread(() =>
+        {
+            while (true)
+            {
+                string msg = reader.ReadLine();
+                if (msg == null) break;
+                Console.WriteLine("Servidor: " + msg);
+            }
+        }).Start();
+
+        while (true)
+        {
+            string msg = Console.ReadLine();
+            writer.WriteLine(msg);
+        }
+    }
+}
---
+++ servidor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        TcpListener servidor = new TcpListener(IPAddress.Parse("127.0.0.1"), 5000);
+
+        servidor.Start();
+        Console.WriteLine("Servidor iniciado... aguardando cliente");
+
+        TcpClient cliente = servidor.AcceptTcpClient();
+        Console.WriteLine("Cliente conectado!");
+
+        NetworkStream stream = cliente.GetStream();
+        StreamReader reader = new StreamReader(stream);
+        StreamWriter writer = new StreamWriter(stream);
+        writer.AutoFlush = true;
+
+        new Thread(() =>
+        {
+            while (true)
+            {
+                string msg = reader.ReadLine();
+                if (msg == null) break;
+                Console.WriteLine("Cliente: " + msg);
+            }
+        }).Start();
+
+        while (true)
+        {
+            string msg = Console.ReadLine();
+            writer.WriteLine(msg);
+        }
+    }
+}
